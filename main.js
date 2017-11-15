f7340652492a33903c77a7589cb5520e19c9407f
Ronnie Hallsten <ronnie.hallsten@gmail.com>
2017-11-15 09:30:03 +0200

    Express test kod.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-console.log("hej")
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
