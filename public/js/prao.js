e6ece344ff9a463b3a8b41b1189ca31eab5be10b
Ronnie Hallsten <ronnie.hallsten@gmail.com>
2017-11-15 10:48:36 +0200

    angular exempel

---
+++ public/js/prao.js
@@ -0,0 +1,21 @@
+var app = angular.module('prao', []);
+
+app.controller('MainCtrl', function ($scope) {
+
+    $scope.details = [
+        {"name": "Employees"},
+        {"name": "Test"},
+        {"name": "Sommar"},
+        {"name": "Support"}
+    ];
+    
+    $scope.add = function(string) {
+        $scope.details.push({ name: string });
+    };
+    
+    $scope.remove = function(e) {
+        console.log('Vi ska ta bort det här:', e);
+        
+        $scope.details.splice(e, 1);
+    };
+});
